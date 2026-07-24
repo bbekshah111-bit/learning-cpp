8e360ef8b0bae9475c03946d47b657a4df0a0667
bbekshah111-bit <bbek.shah111@gmail.com>
2026-07-24 11:40:57 +0545

    add loop and class vector.

--- src/10_looping.cpp
+++ src/10_looping.cpp
@@ -1,13 +1,39 @@
 #include<iostream>
 #include<vector>
+#include<cmath>
 
 using namespace std;
 
-int main()
-{
-    vector <float> a = {1.0, 1.3, 2.4};
-    vector <float> b { 2.3, 3.5, 1.3};
+// int main()
+// {
+//     vector <float> a = {1.0, 1.3, 2.4};
+//     vector <float> b { 2.3, 3.5, 1.3};
 
+//     float dot_product = 0;
+//     float dot;
+
+//     if(a.size() != b.size())
+//     {
+//         cerr << "Invalid input, both matrix must match the size.";
+//         return 1;
+//     }
+
+//     for (size_t i=0; i < a.size(); i++)
+//     {
+//         dot = a[i] * b[i];
+
+//         dot_product += dot;
+
+//         cout << dot << endl;
+//     }
+
+
+//     cout << " sum: " << dot_product;
+
+// }
+
+float dotProduct(vector<float> a, vector<float> b)
+{
     float dot_product = 0;
     float dot;
 
@@ -22,12 +48,18 @@ int main()
         dot = a[i] * b[i];
 
         dot_product += dot;
-
-        cout << dot << endl;
     }
 
+    return dot_product;
+}
 
-    cout << " sum: " << dot_product;
+float magnitude(vector<float> a){
 
 }
+
+int main(){
+    cout << dotProduct({1.2, 3.4, 5.5}, {3, 4.4, 3.2});
+}
+
+
 
